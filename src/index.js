fc1adbc5b86c51dbd294ad7d2f430bc5a74f4ac0
Eugene Kos <mstrsid@live.com>
2022-01-21 10:22:39 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let isOk, bracketsSubstr;
+
+	while (true) {
+		isOk = false;
+		bracketsConfig.forEach(item => {
+			bracketsSubstr = item.join('');
+			if (str.includes(bracketsSubstr)) {
+				str = str.replace(bracketsSubstr, '');
+				isOk = true;
+			}
+		});
+		if (!isOk) {
+			break;
+		}
+	}
+
+	return (str.length === 0) ? true : false;
+};
